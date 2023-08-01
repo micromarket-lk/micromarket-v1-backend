35739dbaeb72f6f268a0ba8383e6e260a48a1d2d
sahanruwanthasilva <sahanr.silva@hotmail.com>
2023-08-01 12:16:29 +0530

    updated market controller in core service

--- core/src/main/java/com/sahan/core/Controllers/MarketController.java
+++ core/src/main/java/com/sahan/core/Controllers/MarketController.java
@@ -1,24 +1,45 @@
 package com.sahan.core.Controllers;
 
+import com.sahan.core.Requests.Market.MarketDeleteRequest;
+import com.sahan.core.Requests.Market.MarketGetRequest;
+import com.sahan.core.Requests.Market.MarketRegistrationRequest;
+import com.sahan.core.Requests.Market.MarketUpdateRequest;
+import com.sahan.core.Services.MarketService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Slf4j
 @RestController
 @RequestMapping("api/v1/market/")
 @AllArgsConstructor
 public class MarketController {
+    MarketService marketService;
+
     @PostMapping("create")
-    public void register(){}
+    public void register(@Valid @RequestBody MarketRegistrationRequest marketRegistrationRequest){
+        log.info("register market {}", marketRegistrationRequest.getMarketName());
+        marketService.createMarket(marketRegistrationRequest);
+    }
     @PutMapping("/uodate")
-    public void update(){}
+    public void update(@Valid @RequestBody MarketUpdateRequest marketUpdateRequest){
+        log.info("update market {}", marketUpdateRequest.getMarketName());
+        marketService.update(marketUpdateRequest);
+    }
     //delete market
     @DeleteMapping("/delete")
-    public void delete(){}
+    public void delete(@Valid @RequestBody MarketDeleteRequest marketDeleteRequest){
+        log.info("delete market {}", marketDeleteRequest.getMarketName());
+        marketService.delete(marketDeleteRequest);
+    }
     //get market by user
     @GetMapping("/get")
-    public void getMarketByUser(){}
+    public void getMarketByUser(@Valid @RequestBody MarketGetRequest marketGetRequest){
+        log.info("get market {}", marketGetRequest.getMarketName());
+        marketService.getMarketByMarketName(marketGetRequest);
+    }
     // all markets
     @GetMapping("/all")
     public void getAllMarkets(){}
--- core/src/main/java/com/sahan/core/Services/MarketService.java
+++ core/src/main/java/com/sahan/core/Services/MarketService.java
@@ -3,11 +3,14 @@ package com.sahan.core.Services;
 import com.sahan.core.Entities.Market.Market;
 import com.sahan.core.Repostitories.MarketRepository;
 import com.sahan.core.Requests.Market.MarketDeleteRequest;
+import com.sahan.core.Requests.Market.MarketGetRequest;
 import com.sahan.core.Requests.Market.MarketRegistrationRequest;
 import com.sahan.core.Requests.Market.MarketUpdateRequest;
 import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
 @AllArgsConstructor
+@Service
 public class MarketService {
 
     private final MarketRepository marketRepository;
@@ -19,8 +22,8 @@ public class MarketService {
     }
 
     // get
-    public void getMarketByMarketName(MarketRegistrationRequest marketRegistrationRequest) {
-        Market market = marketRepository.findMarketByMarketName(marketRegistrationRequest.getMarketName());
+    public void getMarketByMarketName(MarketGetRequest marketGetRequest) {
+        Market market = marketRepository.findMarketByMarketName(marketGetRequest.getMarketName());
     }
 
     // update
